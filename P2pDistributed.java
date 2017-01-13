e1c1f09f19bbd7c88cdb2058e017a6cea90665dd
moallailani <moalla_ilani@hotmail.com>
2017-01-13 18:34:49 +0200

    Add files via upload

---
+++ P2pDistributed.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2pdistributed;
+import p2pdistributed.gui.MainScreen;
+/**
+ *
+ * @author moall
+ */
+public class P2pDistributed {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainScreen screen = new MainScreen();
+        screen.show();
+    }
+    
+}
